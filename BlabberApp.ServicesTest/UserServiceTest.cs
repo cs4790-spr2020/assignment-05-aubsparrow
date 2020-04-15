75ce3e21ffc9c07601d8ca1190a466ea73f6d87f
Aubrey Sparrow <aubreysparrow@mail.weber.edu>
2020-04-14 20:01:30 -0600

    added UserServiceTest

--- BlabberApp.ServicesTest/UserServiceTest.cs
+++ BlabberApp.ServicesTest/UserServiceTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BlabberApp.Services; 
+using BlabberApp.Domain;
 
 namespace BlabberApp.ServicesTest
 {
@@ -8,21 +9,49 @@ namespace BlabberApp.ServicesTest
     public class UserServiceTest
     {
         private UserFactory factory;
+        private UserService service;
+        private string email = "foobar@example.com";
+
         [TestInitialize]
         public void Setup()
         {
             factory = new UserFactory();
+            service = factory.CreateUserService();
         }
         [TestMethod]
         public void TestGetAllUsers()
         {
-            //Arrange
-            UserService userService = factory.CreateUserService();
             ArrayList expected = new ArrayList();
-            //Act
-            IEnumerable actual = userService.GetAllUsers();
-            //Assert
-            Assert.AreEqual(expected.Count, (actual as ArrayList).Count);
-        }   
+            ArrayList actual = (ArrayList)service.GetAllUsers();
+            Assert.AreEqual(expected.Count, actual.Count);
+        } 
+
+        [TestMethod]
+        public void TestAddNewUser()
+        {
+            service.AddNewUser(email);
+            ArrayList allUsers =(ArrayList) service.GetAllUsers();
+            foreach(User user in allUsers)
+            {
+                if(user.Email == email)
+                {
+                    Assert.AreEqual(email, user.Email);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestCreateUser()
+        {
+            service.CreateUser(email);
+            ArrayList allUsers = (ArrayList)service.GetAllUsers();
+            foreach(User user in allUsers)
+            {
+                if(user.Email == email)
+                {
+                    Assert.AreEqual(email, user.Email);
+                }
+            }
+        }  
     }
 }
